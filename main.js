7f1309c8dc1fb8fcbb95f175e4c7c2eae58d7a08
jwku <116755047+jwku@users.noreply.github.com>
2025-08-24 21:44:37 -0600

    to allow "start": "electronmon ." instead of "start": "electron ."
    app.css: 
    package.json: 
    package-lock.json: 
    main.js:

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 
 const {app, BrowserWindow} = require('electron');
 
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
 let mainWindow = null;
 
 app.on('window-all-closed', ()=>{
